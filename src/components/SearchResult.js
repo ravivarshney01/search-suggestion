db80cdfefd0d3e07a7f4131f147b738600dee187
Ravi Varshney <ravivarshney49@gmail.com>
2020-11-12 21:46:24 +0530

    removed extra div

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 const SearchResult = ({ result }) => {
   return (
-    <div>
+    <>
       <p className="text-4xl">{result}</p>
       <Link to="/" className="">
         &#8592; Go back
       </Link>
-    </div>
+    </>
   );
 };
 
